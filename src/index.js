57f0c2f0e991060bf306807fe41dbe7bd20b2593
SyoiWatsu <r.nakano.job@gmail.com>
2023-06-03 08:36:31 +0000

    remove: 不要なコードを削除

--- src/index.js
+++ src/index.js
@@ -1,17 +1,4 @@
 import Operation from 'operation';
-import qiitaApi from 'qiita_api';
-
-// embed arrow function
-global.func2 = () => {
-  const msg = 'hello hello !!';
-  console.log(msg);
-};
-
-global.TEST = () => {
-  const res = qiitaApi.authenticatedUser.items.get();
-  const items = res.body;
-  console.log(items);
-};
 
 global.storeItemDataDaily = () => {
   Operation.storeItemData();

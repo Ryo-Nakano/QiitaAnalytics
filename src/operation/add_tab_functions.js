935590895f6ac0afd217366288571d938d9a952c
SyoiWatsu <r.nakano.job@gmail.com>
2023-07-01 05:41:22 +0000

    update: addTabFunctions

--- src/operation/add_tab_functions.js
+++ src/operation/add_tab_functions.js
@@ -1,17 +1,20 @@
 import { spreadsheet } from "sheets";
 
-const tabFunction = ({name, functionName}) => ({
-  name,
-  functionName,
-});
-
 const addTabFunctions = () => {
+  const tabFunction = ({name, functionName}) => ({
+    name,
+    functionName,
+  });
   const setQiitaApiToken = tabFunction({
     name: 'Qiita API のアクセストークンを設定',
     functionName: 'setQiitaApiToken',
   });
+  const setSpreadsheetId = tabFunction({
+    name: 'スプレッドシートID を設定',
+    functionName: 'setSpreadsheetId',
+  });
 
-  spreadsheet.addMenu('事前準備', [setQiitaApiToken]);
+  spreadsheet.addMenu('各種設定', [setQiitaApiToken, setSpreadsheetId]);
 };
 
 export default addTabFunctions;

1e3899f42f9c42a37016ffe32db17829ee217d1e
SyoiWatsu <r.nakano.job@gmail.com>
2023-07-28 23:27:42 +0000

    remove: 過去の遺産を削除

--- src/sheets.js
+++ src/sheets.js
@@ -58,39 +58,3 @@ function getSpreadSheet() {
 function getLastRow (sheet) {
   return sheet.getDataRange().getValues().filter(row => row[0]).length + 1;
 };
-
-
-// memo: 過去の遺産 === === === === ===
-function showAlertModal() {
-  const ui = SpreadsheetApp.getUi();
-  const title = 'お願い';
-  const body = [
-    'スプレッドシートIDが正しく設定されていない可能性があります。',
-    '',
-    '下の入力欄からスプレッドシートIDの設定をしてください！🙏',
-    '(入力された値はスクリプトプロパティに保存されます)',
-    '　',
-  ].join('\n');
-
-  const result = ui.prompt(title, body, ui.ButtonSet.OK);
-  const input = result.getResponseText();
-
-  return input;
-};
-
-function saveProperty(key, value) {
-  try {
-    const scriptProperties = PropertiesService.getScriptProperties();
-    scriptProperties.setProperty(key, value);
-  }
-  catch(error) {
-    console.error(error);
-    throw Error('failed to save script property ...');
-  }
-};
-
-function showDoneModal() {
-  const ui = SpreadsheetApp.getUi();
-  const title = '保存しました';
-  ui.alert(title, '', ui.ButtonSet.OK);
-};
